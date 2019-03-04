1a9262fa4db1a19ba2d659e8297a191668a73c42
xuqingxin <xuqingxin@xiaohe.com>
2019-03-04 16:28:21 +0800

    提交基础类

---
+++ MSBase/src/test/java/com/eternity/microservice/base/MSBase/MsBaseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eternity.microservice.base.MSBase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsBaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
